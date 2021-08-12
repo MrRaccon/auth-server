da2a493353657067a899a6b7ff168224880e942a
Alexis <luis.sosa@gnp.com.mx>
2021-08-12 14:45:20 -0500

    se termina ejemplo

---
+++ src/main/resources/snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+public class Snippet {
+    public static void main(String[] args) {
+        /home/alexis/Documentos/AUTH_PRUEBA_ESTUDIO/auth-server/src/main/resources
+    }
+}
+
