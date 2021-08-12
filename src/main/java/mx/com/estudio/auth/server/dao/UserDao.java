11aa6b064320ea6e8a6b0b3c7766e81c6ba077b7
Alexis <luis.sosa@gnp.com.mx>
2021-08-12 12:36:16 -0500

    se agrega base de datos en h2

--- src/main/java/mx/com/estudio/auth/server/dao/UserDao.java
+++ src/main/java/mx/com/estudio/auth/server/dao/UserDao.java
@@ -1,11 +1,11 @@
 package mx.com.estudio.auth.server.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import mx.com.estudio.auth.server.model.User;
 
 @Repository
-public interface UserDao extends CrudRepository<User, Long> {
+public interface UserDao extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
